9f411e54c0486ad8cf30246f7cc01c1fad8d8f06
Mark Noonan <markthomasnoonan@gmail.com>
2021-02-06 17:36:45 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 60px auto;
+  max-width: 800px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <h1>John's Things</h1>
+
+  <ul class="things">
+    <li v-for="i in 21" :key="i">
+      <a :href="`#test${i}`">
+        Word {{i}}
+      </a>
+    </li>
+  </ul>
+
+
+
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+  background-color: aliceblue;
+  margin: 5px;
+  height: 200px;
+  widows: 200px;
+  display: grid;
+  place-items: center;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+a:hover {
+  background-color: lightblue;
+  color: black;
+}
+
+.things {
+  list-style-type: none;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+</style>
